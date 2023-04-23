c2407f56342895191c1cec18dea80c19872cfeeb
Sammi Turner <ms.sammi.turner@gmail.com>
2023-04-23 09:44:14 +0100

    Add files via upload

---
+++ Ocaml/fizzbuzz/fizzbuzz.ml
@@ -0,0 +1,14 @@
+let fizzbuzz n =
+  for i = 1 to n do
+    if i mod 3 = 0 && i mod 5 = 0 then
+      print_endline "FizzBuzz"
+    else if i mod 3 = 0 then
+      print_endline "Fizz"
+    else if i mod 5 = 0 then
+      print_endline "Buzz"
+    else
+      Printf.printf "%d\n" i
+  done
+
+let () = fizzbuzz 100
+
