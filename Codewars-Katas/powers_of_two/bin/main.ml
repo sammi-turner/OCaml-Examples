bbacfdefd4df3d3fb0adc5b686a88db8e611989a
Sammi Turner <ms.sammi.turner@gmail.com>
2023-04-25 12:49:07 +0100

    Add files via upload

---
+++ Codewars-Katas/powers_of_two/bin/main.ml
@@ -0,0 +1,19 @@
+(* Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ). *)
+
+(* Examples *)
+
+(* n = 0  ==> [1] *)
+(* n = 1  ==> [1, 2] *)
+(* n = 2  ==> [1, 2, 4] *)
+
+let powersOfTwo n = List.init (n + 1) ((lsl) 1)
+
+let a = powersOfTwo 0
+let b = powersOfTwo 1
+let c = powersOfTwo 2
+let () = List.iter (Printf.printf "%d ") a
+let () = Printf.printf("\n")
+let () = List.iter (Printf.printf "%d ") b
+let () = Printf.printf("\n")
+let () = List.iter (Printf.printf "%d ") c
+let () = Printf.printf("\n")
