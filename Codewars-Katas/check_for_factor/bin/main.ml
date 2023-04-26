256ec18df18bfbb5f88265ae30e3bee8f91defb6
Sammi Turner <ms.sammi.turner@gmail.com>
2023-04-26 23:25:10 +0100

    Update main.ml

--- Codewars-Katas/check_for_factor/bin/main.ml
+++ Codewars-Katas/check_for_factor/bin/main.ml
@@ -1,18 +1,16 @@
 (* 
 
-Write a function should test if a number is a factor of a base.
+Write a function to test if a number is a factor of a given base.
 
-Return true if it is a factor or false if it is not.
-
-Factors are numbers you can multiply together to get another number.
+Return true if it is a factor, or false if it is not.
 
 Examples:
 
-2 and 3 are factors of 6 because: 2 * 3 = 6
+2 and 3 are factors of 6 because 2 * 3 = 6
 
-2 is not a factor of 7 because: 7 mod 2 = 1
+2 is not a factor of 7 because 7 mod 2 = 1
 
-Note: base is a non-negative number, factor is a positive number.
+Note that base is a non-negative number and factor is a positive number.
 
 *)
 
@@ -21,6 +19,7 @@ let check_for_factor (base: int) (factor: int): bool = (base mod factor) == 0
 let test0 = check_for_factor 10 2
 let test1 = check_for_factor 63 7
 let test2 = check_for_factor 9 2
+
 let () = Printf.printf "%b\n" test0
 let () = Printf.printf "%b\n" test1
 let () = Printf.printf "%b\n" test2
