0a51fde0189246dc7fab750c6d5821cdac28ab17
Sammi Turner <ms.sammi.turner@gmail.com>
2023-04-26 23:15:56 +0100

    Update main.ml

--- Codewars-Katas/opposite_number/bin/main.ml
+++ Codewars-Katas/opposite_number/bin/main.ml
@@ -1,8 +1,15 @@
-(* Given an integer or a floating-point number, find its opposite. Examples :
-* 1 => -1
-* 14 => -14
-* -34 => 34
+(* 
+
+Given an integer or a floating-point number, find its opposite. 
+
+Examples:
+
+1 => -1
+14 => -14
+-34 => 34
+
 *)
+
 let opposite (number : int) : int =
   if number = 0 then 0 else (-1) * number
 
