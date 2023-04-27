b5a548adbfd119a26df5737c59cbac99f8b219db
Sammi Turner <ms.sammi.turner@gmail.com>
2023-04-27 13:47:44 +0100

    Add files via upload

---
+++ Codewars-Katas/summation/bin/main.ml
@@ -0,0 +1,25 @@
+(*
+
+Write a program that finds the summation of every number from 1 to num.
+
+The number will always be a positive integer greater than 0.
+
+Example:
+
+2 => 3
+3 => 6
+8 => 36
+
+*)
+
+let rec summation (n: int): int =
+  if n <= 0 then 0
+  else n + summation (n - 1)
+
+let test0 = summation 2
+let test1 = summation 3
+let test2 = summation 8
+
+let () = Printf.printf "%d\n" test0
+let () = Printf.printf "%d\n" test1
+let () = Printf.printf "%d\n" test2
