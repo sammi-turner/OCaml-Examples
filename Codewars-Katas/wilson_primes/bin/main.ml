d9d22549a328d8d81c8263ffc0102b20d380024a
Sammi Turner <ms.sammi.turner@gmail.com>
2023-04-27 16:26:30 +0100

    Update main.ml

--- Codewars-Katas/wilson_primes/bin/main.ml
+++ Codewars-Katas/wilson_primes/bin/main.ml
@@ -1,14 +1,8 @@
 (*
 
-Wilson primes satisfy the following condition. Let P represent a prime number.
+Create a function that returns true if the given number is a Wilson prime.
 
-Then,
-
-((P-1)! + 1) / (P * P)
-
-should give a whole number.
-
-Your task is to create a function that returns true if the given number is a Wilson prime.
+https://t5k.org/glossary/page.php?sort=WilsonPrime
 
 *)
 
