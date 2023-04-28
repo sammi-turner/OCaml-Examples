5b02ab3716eb4ccbce6ab5e66570858148abb8d6
Sammi Turner <ms.sammi.turner@gmail.com>
2023-04-28 19:23:44 +0100

    Update main.ml

--- Codewars-Katas/is_palindrome/bin/main.ml
+++ Codewars-Katas/is_palindrome/bin/main.ml
@@ -1,6 +1,9 @@
 (*
    
-Write a function that checks if a given string (case insensitive) is a palindrome. A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar, the date and time 12/21/33 12:21, and the sentence: "A man, a plan, a canal – Panama".
+Write a function that checks if a given string (case insensitive) is a palindrome. 
+
+A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, 
+such as madam or racecar, the date and time 12/21/33 12:21, and the sentence: "A man, a plan, a canal – Panama".
 
 Examples:
 
