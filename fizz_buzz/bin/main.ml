64c70ab386e6e1dcda03c2cd5b2475c7580b32eb
Sammi Turner <ms.sammi.turner@gmail.com>
2023-04-29 21:30:34 +0100

    Update main.ml

--- fizz_buzz/bin/main.ml
+++ fizz_buzz/bin/main.ml
@@ -1,13 +1,10 @@
 let fizzbuzz n =
   for i = 1 to n do
-    if i mod 3 = 0 && i mod 5 = 0 then
-      print_endline "FizzBuzz"
-    else if i mod 3 = 0 then
-      print_endline "Fizz"
-    else if i mod 5 = 0 then
-      print_endline "Buzz"
-    else
-      Printf.printf "%d\n" i
+    match i mod 3, i mod 5 with
+    | 0, 0 -> print_endline "FizzBuzz"
+    | 0, _ -> print_endline "Fizz"
+    | _, 0 -> print_endline "Buzz"
+    | _, _ -> Printf.printf "%d\n" i
   done
 
 let () = fizzbuzz 100
