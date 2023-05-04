c4e6ca613a183453048e1b0c397e49874c2edaf8
Sammi Turner <ms.sammi.turner@gmail.com>
2023-05-04 01:12:29 +0100

    Add files via upload

---
+++ Codewars-Katas/build_a_square/bin/main.ml
@@ -0,0 +1,29 @@
+(*
+   
+Write a function that takes an integer and returns a square of plus symbols as long and wide as the integer. 
+
+The integer will be a whole number between 1 and 50.
+
+Example:
+
+n = 3, so I expect a 3x3 square back just like below as a string.
+
++++
++++
++++
+
+*)
+
+let generate_shape (n: int): string =
+  List.init n (fun _ -> String.make n '+')
+  |> String.concat "\n"
+
+let test0 = generate_shape 5
+let test1 = generate_shape 3
+let test2 = generate_shape 1
+
+let () = print_endline test0
+let () = print_endline ""
+let () = print_endline test1
+let () = print_endline ""
+let () = print_endline test2
