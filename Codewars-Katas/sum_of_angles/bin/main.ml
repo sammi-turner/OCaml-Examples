18cb1c58114a45d7cc2b664f54736cc0040b6b9c
Sammi Turner <ms.sammi.turner@gmail.com>
2023-05-10 15:28:43 +0100

    Add files via upload

---
+++ Codewars-Katas/sum_of_angles/bin/main.ml
@@ -0,0 +1,21 @@
+(*
+   
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon.
+
+n will be greater than 2.
+
+Examples:
+
+3 => 180
+4 => 360
+5 => 540
+
+*)
+
+let angle n = (n - 2) * 180
+
+let test3 = angle 3
+let test4 = angle 4
+let test5 = angle 5
+
+let () = Printf.printf "%d %d %d\n" test3 test4 test5
