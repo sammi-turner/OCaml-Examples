ccaccf4140ade66c8fd2ce3c84cc117de9b545e7
Sammi Turner <ms.sammi.turner@gmail.com>
2023-05-14 19:04:56 +0100

    Add files via upload

--- utils/bin/main.ml
+++ utils/bin/main.ml
@@ -1,4 +1,6 @@
 open Utils
 
-let test = ["Mary";"had";"a";"little";"lamb"]
-let () = print_list test
+let test0 = ["had";"a";"little"]
+let test1 = "Mary" :: test0
+let test2 = test1 @ ["lamb."]
+let () = print_list test2
