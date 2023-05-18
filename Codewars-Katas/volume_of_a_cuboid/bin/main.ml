90becde322d6fea805ac7fa553b6526f4d7f859e
Sammi Turner <ms.sammi.turner@gmail.com>
2023-05-18 21:01:18 +0100

    Add files via upload

--- Codewars-Katas/volume_of_a_cuboid/bin/main.ml
+++ Codewars-Katas/volume_of_a_cuboid/bin/main.ml
@@ -4,11 +4,11 @@ Write a function to calculate the volume of a cuboid with three values: the leng
 
 *)
 
-let get_volume_of_cuboid (length: int) (width: int) (height: int): int =
+let get_volume_of_cuboid (length:int) (width:int) (height:int) =
   length * width * height
 
-let r0 = get_volume_of_cuboid 3 5 7
-let r1 = get_volume_of_cuboid 2 4 1
-let r2 = get_volume_of_cuboid 3 4 6
+let test0 = get_volume_of_cuboid 3 5 7
+let test1 = get_volume_of_cuboid 2 4 1
+let test2 = get_volume_of_cuboid 3 4 6
 
 let () = Printf.printf "%d\n%d\n%d\n" test0 test1 test2
