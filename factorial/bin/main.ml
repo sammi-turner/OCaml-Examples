605cce75070c3d76fb7fbc8d9014b391b7aa18e7
Sammi Turner <ms.sammi.turner@gmail.com>
2023-06-04 02:57:14 +0100

    Add files via upload

---
+++ factorial/bin/main.ml
@@ -0,0 +1,11 @@
+let rec factorial = function
+  | 0 -> 1
+  | n -> n * factorial (n - 1)
+
+let test0 = factorial 0
+let test1 = factorial 1
+let test2 = factorial 2
+let test3 = factorial 3
+let test4 = factorial 4
+
+let () = Printf.printf "%i %i %i %i %i\n" test0 test1 test2 test3 test4
