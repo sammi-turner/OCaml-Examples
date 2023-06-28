efca80c4b81a2a41283d166245440003dc0bf0b5
Sammi Turner <ms.sammi.turner@gmail.com>
2023-06-28 13:43:46 +0100

    Add files via upload

--- fizz_buzz/bin/main.ml
+++ fizz_buzz/bin/main.ml
@@ -1,10 +1,10 @@
-let fizzbuzz n =
+let fizzbuzz (n:int) : unit =
   for i = 1 to n do
     match i mod 3, i mod 5 with
     | 0, 0 -> print_endline "FizzBuzz"
     | 0, _ -> print_endline "Fizz"
     | _, 0 -> print_endline "Buzz"
     | _, _ -> Printf.printf "%d\n" i
-  done
+  done;;
 
-let () = fizzbuzz 100
+fizzbuzz 100
