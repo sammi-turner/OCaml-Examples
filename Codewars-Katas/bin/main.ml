1824580e52c5a116a537d3be932937e95bbd2284
Sammi Turner <ms.sammi.turner@gmail.com>
2023-06-28 13:23:37 +0100

    Add files via upload

---
+++ Codewars-Katas/bin/main.ml
@@ -0,0 +1,9 @@
+(* 
+
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?"
+
+*)
+
+let greet (name:string) = "Hello, " ^ name ^ " how are you doing today?"
+let s = greet "Mary";;
+print_endline s
