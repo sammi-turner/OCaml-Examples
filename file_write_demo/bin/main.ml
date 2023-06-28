36f23dc46e678d57dfb150ac5a1c51da52192559
Sammi Turner <ms.sammi.turner@gmail.com>
2023-06-28 13:41:45 +0100

    Add files via upload

--- file_write_demo/bin/main.ml
+++ file_write_demo/bin/main.ml
@@ -1,6 +1,6 @@
-let write_file name content =
+let write_file (name:string) (content:string) : unit =
   let oc = open_out name in
   output_string oc content;
-  close_out oc
+  close_out oc;;
 
-let () = write_file "example.txt" "Hello, world!"
+write_file "example.txt" "Oh, hi Mark!"
