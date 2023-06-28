3385c77200268e8d98f6703a90493f4ee157bb46
Sammi Turner <ms.sammi.turner@gmail.com>
2023-06-28 13:26:27 +0100

    Add files via upload

--- Codewars-Katas/sum_of_positive/bin/main.ml
+++ Codewars-Katas/sum_of_positive/bin/main.ml
@@ -10,13 +10,13 @@ Examples:
 
 *)
 
-let positive_sum (lst:int list) =
+let positive_sum (lst:int list) : int =
   let sum = ref 0 in
   List.iter (fun x -> if x > 0 then sum := !sum + x) lst;
   !sum
 
 let test0 = positive_sum []
 let test1 = positive_sum [0;-3]
-let test2 = positive_sum [1;-4;7;12]
+let test2 = positive_sum [1;-4;7;12];;
 
-let () = Printf.printf "%d\n%d\n%d\n" test0 test1 test2
+Printf.printf "%d\n%d\n%d\n" test0 test1 test2
