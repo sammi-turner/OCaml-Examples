00f6ded012fb1ceead5c029d20b70a55bb5cf2d8
Sammi Turner <ms.sammi.turner@gmail.com>
2023-06-28 13:37:08 +0100

    Add files via upload

--- factorial/bin/main.ml
+++ factorial/bin/main.ml
@@ -6,6 +6,6 @@ let test0 = factorial 0
 let test1 = factorial 1
 let test2 = factorial 2
 let test3 = factorial 3
-let test4 = factorial 4
+let test4 = factorial 4;;
 
-let () = Printf.printf "%i %i %i %i %i\n" test0 test1 test2 test3 test4
+Printf.printf "%i %i %i %i %i\n" test0 test1 test2 test3 test4
