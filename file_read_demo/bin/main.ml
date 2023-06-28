01e1f506992b292b89f200b6dc2d7d5e31f32cc3
Sammi Turner <ms.sammi.turner@gmail.com>
2023-06-28 13:36:12 +0100

    Add files via upload

--- file_read_demo/bin/main.ml
+++ file_read_demo/bin/main.ml
@@ -1,11 +1,17 @@
-let read_file name =
+let read_file (name:string) : string option =
   try
     let channel = open_in name in
     let contents = really_input_string channel (in_channel_length channel) in
     close_in channel;
-    contents
+    Some (contents)
   with
-    | Sys_error _ -> ""
+    | Sys_error _ -> None
 
-let content = read_file "example.txt"
-let () = print_endline content
+let print_file_contents (x:string option) =
+  match x with
+  | Some x -> Printf.printf "File contents : %s\n" x
+  | None -> Printf.printf "That file does not exist in the working directory.\n"
+
+let file = "example.txt"
+let content = read_file file;;
+print_file_contents content
