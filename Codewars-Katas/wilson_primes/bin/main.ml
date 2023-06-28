a268b188e0737ccba3ad0a59273b7b37678f7d5f
Sammi Turner <ms.sammi.turner@gmail.com>
2023-06-28 13:28:51 +0100

    Add files via upload

--- Codewars-Katas/wilson_primes/bin/main.ml
+++ Codewars-Katas/wilson_primes/bin/main.ml
@@ -6,10 +6,10 @@ https://t5k.org/glossary/page.php?sort=WilsonPrime
 
 *)
 
-let i_am_a_wilson_prime (n:int) = n == 5 || n == 13 || n == 563
+let i_am_a_wilson_prime (n:int) : bool = n == 5 || n == 13 || n == 563
 
 let test0 = i_am_a_wilson_prime 2
 let test1 = i_am_a_wilson_prime 3
-let test2 = i_am_a_wilson_prime 5
+let test2 = i_am_a_wilson_prime 5;;
 
-let () = Printf.printf "%b\n%b\n%b\n" test0 test1 test2
+Printf.printf "%b\n%b\n%b\n" test0 test1 test2
