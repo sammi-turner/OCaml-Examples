23a5e6ee08ce9f4b0038be9ccfff51896d3a02f9
Sammi Turner <ms.sammi.turner@gmail.com>
2023-09-24 01:23:27 +0100

    Update main.ml

--- cli_args_demo/bin/main.ml
+++ cli_args_demo/bin/main.ml
@@ -1,5 +1,5 @@
 let () =
   let argc = Array.length Sys.argv in
   for i = 1 to argc - 1 do
-    Printf.printf "Argument %d: %s\n" i Sys.argv.(i)
+    Printf.printf " %d. %s |" i Sys.argv.(i)
   done
