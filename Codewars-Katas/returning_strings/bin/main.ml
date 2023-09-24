0f8f274656be0d2f87decae9e345d62f72e6e412
Sammi Turner <ms.sammi.turner@gmail.com>
2023-09-24 14:59:49 +0100

    Update main.ml

--- Codewars-Katas/returning_strings/bin/main.ml
+++ Codewars-Katas/returning_strings/bin/main.ml
@@ -5,5 +5,4 @@ Make a function that will return a greeting statement that uses an input; your p
 *)
 
 let greet (name:string) = "Hello, " ^ name ^ " how are you doing today?"
-let s = greet "Mary";;
-print_endline s
+let () = print_endline (greet "Mary")
