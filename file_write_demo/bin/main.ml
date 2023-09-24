97a0a729d21ac2935e783839d444a6ae2891e346
Sammi Turner <ms.sammi.turner@gmail.com>
2023-09-24 01:30:38 +0100

    Update main.ml

--- file_write_demo/bin/main.ml
+++ file_write_demo/bin/main.ml
@@ -1,6 +1,6 @@
 let write_file (name:string) (content:string) : unit =
   let oc = open_out name in
   output_string oc content;
-  close_out oc;;
+  close_out oc
 
-write_file "example.txt" "Oh, hi Mark!"
+let () = write_file "example.txt" "Oh, hi Mark!"
