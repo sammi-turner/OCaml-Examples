1b44b3843846d5c6c80ca4706bb15e87395e6d07
Sammi Turner <ms.sammi.turner@gmail.com>
2023-09-24 15:11:17 +0100

    Update main.ml

--- Codewars-Katas/volume_of_a_cuboid/bin/main.ml
+++ Codewars-Katas/volume_of_a_cuboid/bin/main.ml
@@ -9,6 +9,5 @@ let get_volume_of_cuboid (length:int) (width:int) (height:int) : int =
 
 let test0 = get_volume_of_cuboid 3 5 7
 let test1 = get_volume_of_cuboid 2 4 1
-let test2 = get_volume_of_cuboid 3 4 6;;
-
-Printf.printf "%d\n%d\n%d\n" test0 test1 test2
+let test2 = get_volume_of_cuboid 3 4 6
+let () = Printf.printf "%d %d %d\n" test0 test1 test2
