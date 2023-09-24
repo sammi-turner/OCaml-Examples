5bc719ddd55584e6f25386754442d80bf90b8bfe
Sammi Turner <ms.sammi.turner@gmail.com>
2023-09-24 15:12:41 +0100

    Update main.ml

--- Codewars-Katas/wilson_primes/bin/main.ml
+++ Codewars-Katas/wilson_primes/bin/main.ml
@@ -10,6 +10,5 @@ let i_am_a_wilson_prime (n:int) : bool = n == 5 || n == 13 || n == 563
 
 let test0 = i_am_a_wilson_prime 2
 let test1 = i_am_a_wilson_prime 3
-let test2 = i_am_a_wilson_prime 5;;
-
-Printf.printf "%b\n%b\n%b\n" test0 test1 test2
+let test2 = i_am_a_wilson_prime 5
+let () = Printf.printf "%b %b %b\n" test0 test1 test2
