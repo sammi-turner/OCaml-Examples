ce12e39cc4ad652dc892a4950039b7ad1739b90d
Sammi Turner <ms.sammi.turner@gmail.com>
2023-09-24 01:28:27 +0100

    Update main.ml

--- file_read_demo/bin/main.ml
+++ file_read_demo/bin/main.ml
@@ -9,9 +9,9 @@ let read_file (name:string) : string option =
 
 let print_file_contents (x:string option) =
   match x with
-  | Some x -> Printf.printf "File contents : %s\n" x
-  | None -> Printf.printf "That file does not exist in the working directory.\n"
+  | Some x -> print_string x
+  | None -> print_string "That file does not exist in the working directory."
 
 let file = "example.txt"
-let content = read_file file;;
-print_file_contents content
+let content = read_file file
+let () = print_file_contents content
