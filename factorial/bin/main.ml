0363ad10b09e290af4f656d6b097ef2fa27906b1
Sammi Turner <ms.sammi.turner@gmail.com>
2024-08-24 23:46:24 +0100

    Update main.ml

--- factorial/bin/main.ml
+++ factorial/bin/main.ml
@@ -2,10 +2,4 @@ let rec factorial = function
   | 0 -> 1
   | n -> n * factorial (n - 1)
 
-let test0 = factorial 0
-let test1 = factorial 1
-let test2 = factorial 2
-let test3 = factorial 3
-let test4 = factorial 4
-
-let () = Printf.printf "%i %i %i %i %i" test0 test1 test2 test3 test4
+let () = Printf.printf "%i %i %i %i %i\n" (factorial 0) (factorial 1) (factorial 2) (factorial 3) (factorial 4)
